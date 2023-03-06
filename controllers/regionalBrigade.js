fd5c190d125f8af63d3353a1c0bf97a4d72eba66
מיה אלטשולר <u9196707@bsmch.net>
2023-03-06 13:38:11 +0200

    add regionalBrigade

---
+++ controllers/regionalBrigade.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const regionalBrigadeService = require('../services/regionalBrigadeService')
+
+router.get('', async (req, res) => {
+	const regionalBrigade = await regionalBrigadeService.getRegionalBrigade();
+
+	res.send(regionalBrigade);
+});
+
+module.exports = router;
