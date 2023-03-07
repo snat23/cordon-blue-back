4f05e975c497bf6d44f9ad8d5222dc9c486e3a09
מיה אלטשולר <u9196707@bsmch.net>
2023-03-07 09:36:48 +0200

    change mongo url

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 
 const dbName = 'medori';
 const mongodbUrl =
-  'mongodb+srv://Sampan96:Rc5ZhVmSRYUZFs94@cluster0.gjr1yzm.mongodb.net/medori?retryWrites=true&w=majority';
+  'mongodb://10.252.30.68:27017/';
 
 let mongodb;
 
