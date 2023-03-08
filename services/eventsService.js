02314793a2c808b09a52cb28434b52690da451ea
מרק שקריבה <u9451154@bsmch.net>
2023-03-08 10:10:46 +0200

    add logs to eventTypes

--- services/eventsService.js
+++ services/eventsService.js
@@ -1,21 +1,25 @@
 const eventsRepository = require("../repositories/eventsRepository");
-const logger = require('../logger.js')
+const logger = require("../logger.js");
 
 exports.getEvents = () => {
   return eventsRepository.getAllEvents();
 };
 
 exports.addEvent = async (event) => {
+  logger.info("Adding event with name:" + event.alertName);
   await eventsRepository.postEvent(event);
 };
 
 exports.findEventById = (id) => {
   logger.info("Searching for event id " + id);
   const event = eventsRepository.findById(id);
-  logger.info("Found event name " + event.alertName  + " for id " + id);
+  logger.info("Found event name " + event.alertName + " for id " + id);
   return event;
 };
 
 exports.updateEventById = (id) => {
-  return eventsRepository.updateById(id);
+  logger.info("Updating for event id " + id);
+  const eventToUpdate = eventsRepository.updateById(id);
+  logger.info("Found event name " + eventToUpdate.alertName + " for id " + id);
+  return eventToUpdate;
 };
