da0b637e11443496e895ca0c6be188a104924eb9
ליהיא פנירי <u9231363@bsmch.net>
2023-03-08 11:07:49 +0200

    add requests from db- get by id

--- services/regionalBrigadeService.js
+++ services/regionalBrigadeService.js
@@ -4,6 +4,10 @@ exports.getRegionalBrigade = () => {
   return regionalBrigadeRepository.getAllRegionalBrigades();
 };
 
+exports.getSectorOfEventById = (id) => {
+  return regionalBrigadeRepository.getSectorById(id);
+};
+
 exports.getSectorByName = (name) => {
     return regionalBrigadeRepository.getSectorsTypeByName(name);
   };
--- services/weaponTypeService.js
+++ services/weaponTypeService.js
@@ -4,6 +4,10 @@ exports.getWeaponType = () => {
   return weaponTypeRepository.getAllWeaponTypes();
 };
 
+exports.getWeaponById = (id) => {
+  return weaponTypeRepository.getWeaponTypeById(id);
+};
+
 exports.getWeaponByName = (name) => {
   return weaponTypeRepository.getWeaponTypeByName(name)
 };
